15a2767f0e5f9d987aa878cd7228fe72ec2e0ae1
rodrigomint <rodrigomint@rodrigomint-VirtualBox.(none)>
2013-02-13 11:37:28 +0000

    Vectoresjava

---
+++ src/VectoresJava.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VectoresJava {
+	
+	ArrayList<Integer> array = new ArrayList<Integer>();
+	
+	VectoresJava(String nombreFichero) {
+		try {
+			Scanner scanner = new Scanner(new File(nombreFichero));
+			while(scanner.hasNextInt())
+				array.add(scanner.nextInt());
+			scanner.close();
+		}
+		catch(FileNotFoundException fnfe) {
+			System.out.println("Fichero no encontrado");
+	    }
+
+	}
+	
+	public int rango() {
+		int min = 94500, max = 0;
+		for(int i = 0; i < array.size(); i++) {
+			if(array.get(i) > max)
+				max = array.get(i);
+			if(array.get(i) < min)
+				min = array.get(i);
+		}
+		return max - min;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		try {
+			VectoresJava objeto = new VectoresJava(args[0]);
+			System.out.println(objeto.rango());
+		}
+		catch(ArrayIndexOutOfBoundsException aioobe) {
+			System.out.println("No se ha dado un nombre de fichero.");
+		}
+
+	}
+
+}
